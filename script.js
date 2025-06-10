8b9d03350adacba4770be7351f5c4fb988e320e3
ChristyTheresaChacko <christychacko9856@gmail.com>
2025-06-10 22:17:53 +0530

    Home page completed.

---
+++ script.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector("#hamburger")
+const menu = document.querySelector("#menu")
+const h1 = document.querySelector("#h1")
+const h2 = document.querySelector("#h2")
+const h3 = document.querySelector("#h3")
+const hlinks=document.querySelectorAll("#hlink")
+const body=document.querySelector("body")
+const moon=document.querySelector("#moon")
+
+
+hamburger.addEventListener("click",()=>{
+    menu.classList.toggle("hidden")
+    h1.classList.toggle("bg-white")
+    h2.classList.toggle("bg-white")
+    h3.classList.toggle("bg-white")
+})
+
+hlinks.forEach(link=>{
+    link.addEventListener("click",()=>{
+    menu.classList.toggle("hidden")
+    h1.classList.toggle("bg-white")
+    h2.classList.toggle("bg-white")
+    h3.classList.toggle("bg-white")
+    
+})
+})
